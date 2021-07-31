4060650285d66e1e2764a10bcd326c42a47c308f
Andre Leme <andresleme@gmail.com>
2021-07-30 21:54:46 -0300

    Big commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+//Exercicio feito na mentoria Dojo (30/7)
+
 import React from 'react';
 import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom'
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import PokemonDetails from './components/PokemonDetails';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <BrowserRouter>
+      <Route exact path='/'
+        render={ (props) => <Pokedex {...props} pokemons={ pokemons } /> }
+      />
+      <Route path='/pokemons/:id'
+       render={ (props) => <PokemonDetails {...props} details={ pokemons } /> } />
+      </BrowserRouter>
     </div>
   );
 }
